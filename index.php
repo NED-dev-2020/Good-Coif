df51fee28c560c5162448f1891b65c702ace223b
NED-dev-2020 <71898174+NED-dev-2020@users.noreply.github.com>
2022-02-11 17:49:13 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,8 @@
 	
 <link rel="stylesheet" type="text/css" href="static/fontawesome/css/all.min.css">
 	
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+	
 	
 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8150629877562817"
      crossorigin="anonymous"></script>
